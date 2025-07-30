42286c47c41e1785c6b1ebc6464ca102805afa27
hassanzahid27 <hz366651@gmail.com>
2025-07-30 11:17:10 +0500

    gap in cards

--- src/components/PricingPlans.jsx
+++ src/components/PricingPlans.jsx
@@ -31,7 +31,7 @@ const PricingPlans = () => {
       </div>
 
       {/* Pricing Cards */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-3 max-w-[1142px] mx-auto mb-16 ">
+      <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-[46px] max-w-[1142px] mx-auto mb-16 ">
         {/* Starter */}
         <div className="rounded-xl shadow-sm border border-gray-200 p-6 flex flex-col h-[471px] w-[350px]">
           <div className="flex-1">
